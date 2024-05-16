9fc23b590d5d89f1bcca5dcf6c11db5c9a30a9e0
Hassiel Casañas <70794290+xHassielx@users.noreply.github.com>
2024-05-16 11:43:43 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -28,7 +28,7 @@ function callApiRequest() {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://54.165.202.163/iot-car-control/back-end/apis/getRegistro.php"
+      "https://54.165.202.163/iot-car-control/back-end/apis/getRegistro.php?valorEstatus="
     )
     .then(function (response) {
       // manejar respuesta exitosa
